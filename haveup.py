585914454374f414f02689204e977be4aef0ea4c
Jakub Skokan <jakub.skokan@havefun.cz>
2014-12-29 11:14:51 +0100

    Remove PySide, use ConfigParser, scp

--- haveup.py
+++ haveup.py
@@ -15,20 +15,20 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
-import notify2
 import argparse
 import string
 import hashlib
 import os
-from subprocess import Popen, PIPE
-from PySide.QtCore import *
-from PySide.QtNetwork import *
+import subprocess
+import configparser
 
-class HaveUp(QObject):
+class HaveUp(object):
 	def __init__(self, args):
 		super(HaveUp, self).__init__()
 		
 		self.files = args.file
+		self.config = configparser.ConfigParser()
+		self.config.read('/home/aither/.config/HaveFun.cz/HaveUp.conf')
 		
 		if len(self.files) < 1:
 			print("Please specify file name")
@@ -36,130 +36,87 @@ class HaveUp(QObject):
 		
 		self.has_more = len(self.files) > 1
 		self.links = []
-		self.settings = QSettings()
-		self.file_class = self.load_group()
 		
-		if args.file_class != "General":
-			for grp in self.settings.childGroups():
-				if grp.startswith(args.file_class):
-					self.file_class.update(self.load_group(grp))
+		if args.file_class == 'DEFAULT' or self.config.has_section(args.file_class):
+			self.file_class = self.load_group(args.file_class)
+		else:
+			for s in self.config.sections():
+				if s.startswith(args.file_class):
+					self.file_class = self.load_group(s)
 					break
+			else:
+				print("Group '{0}' not found".format(args.file_class), file=sys.stderr)
+				sys.exit(1)
 		
 		if args.subdir:
 			self.subdir = "/" + args.subdir
-		elif "SubDir" in self.file_class:
-			self.subdir = "/" + str(self.file_class["SubDir"])
+		elif "subdir" in self.file_class:
+			self.subdir = "/" + self.file_class["subdir"]
 		else:
 			self.subdir = ""
 		
 		if args.hash:
 			self.hash = args.hash
-		elif "HashName" in self.file_class:
-			self.hash = bool(self.file_class["HashName"])
+		elif "hashname" in self.file_class:
+			self.hash = self.config[self.cfg_section].getboolean("hashname")
 		else:
 			self.hash = False
 		
-		self.manager = QNetworkAccessManager(self)
-		self.manager.finished.connect(self.uploadFinished)
-		self.manager.authenticationRequired.connect(self.authenticate)
-		
-		self.uploadFile()
+		self.uploadFiles()
 	
 	def load_group(self, grp = None):
 		ret = {}
+		self.cfg_section = grp or 'DEFAULT'
+		grp = self.config[self.cfg_section]
 		
-		if grp:
-			self.settings.beginGroup(grp)
-		
-		for key in self.settings.childKeys():
-			ret[key] = self.settings.value(key)
-		
-		if grp:
-			self.settings.endGroup()
+		for key in grp:
+			ret[ key ] = grp[ key ]
 		
 		return ret
 	
-	def uploadFile(self):
-		self.tries = 0
+	def uploadFiles(self):
+		for f in self.files:
+			base_name = os.path.basename(f)
+			
+			if self.hash:
+				parts = base_name.split('.')
+				
+				if len(parts) > 1:
+					suffix = '.' + parts[-1]
+				else:
+					suffix = ''
+				
+				target_name = hashlib.sha1(bytes(base_name, "UTF-8")).hexdigest() + suffix
+			else:
+				target_name = base_name
+			
+			dl_url = self.file_class["publicurl"] + self.subdir + "/" + target_name
+			upload_to = self.file_class["uploadurl"] + "/" + self.subdir + "/" + target_name
+			
+			if subprocess.call(['scp', f, upload_to], shell=False) == 0:
+				self.uploadFinished(f, dl_url)
 		
-		if len(self.files) == 0:
-			exit(0)
-			return
-		
-		self.cur_file = QFile(self.files.pop(0))
-		base_name = os.path.basename(self.cur_file.fileName())
-		
-		if not self.cur_file.open(QIODevice.ReadOnly):
-			print("Unable to open", self.cur_file.fileName())
-			self.uploadFile()
-			return
-		
-		if self.hash:
-			target_name = hashlib.sha1(bytes(self.cur_file.fileName(), "UTF-8")).hexdigest() + "." + base_name.split('.')[-1]
-		else:
-			target_name = base_name
-		
-		self.url = str(self.settings.value("PublicUrl")) + self.subdir + "/" + target_name
-		
-		self.request = QNetworkRequest(QUrl(str(self.settings.value("UploadUrl")) + self.subdir + "/" + target_name))
-		self.reply = self.manager.put(self.request, self.cur_file)
-		self.reply.error.connect(self.errorOccured)
 	
-	@Slot(QNetworkReply, QAuthenticator)
-	def authenticate(self, reply, auth):
-		self.tries += 1
+	def uploadFinished(self, f, url):
+		print("{0}: {1}".format(f, url))
 		
-		if self.tries > 3:
-			print("Authentication failed")
-			self.reply.abort()
-			self.reply.deleteLater()
-			exit(2)
-			return
+		self.links.append(url)
 		
-		auth.setUser(str(self.settings.value("User")))
-		auth.setPassword(str(self.settings.value("Password")))
+		try:
+			p = subprocess.Popen(['xsel', '-pi'], stdin=subprocess.PIPE, shell=False)
+			p.communicate(bytes("\n".join(self.links), "UTF-8"))
+			p.wait()
+			
+		except OSError as e:
+			if e.errno != os.errno.ENOENT:
+				raise
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description="Upload file(s) and print links")
+	parser.add_argument('-c', '--class', dest='file_class', default='DEFAULT', help="Use settings from specified class")
+	parser.add_argument('-d', '--dir', dest='subdir', default='', help="Upload files to specified subdirectory")
+	parser.add_argument('-s', '--hash-name', action='store_true', dest='hash', help="Hash file name for every file")
+	parser.add_argument('file', metavar='FILE', nargs='+', help="File(s) to upload")
+	args = parser.parse_args()
 	
-	@Slot(QNetworkReply.NetworkError)
-	def errorOccured(self, err):
-		print("Error occured:", err)
-	
-	def uploadFinished(self):
-		self.cur_file.close()
-		self.reply.deleteLater()
-		
-		notify2.Notification(
-			"File uploaded" if self.has_more and len(self.files) > 0 else "Upload finished",
-			"File <b>%s</b> was successfuly uploaded" % self.cur_file.fileName()
-		).show()
-		
-		print("{0}: {1}".format(self.cur_file.fileName(), self.url))
-		
-		self.links.append(self.url)
-		
-		p = Popen(['xsel', '-pi'], stdin=PIPE, shell=True)
-		p.communicate(bytes("\n".join(self.links), "UTF-8"))
-		p.wait()
-		
-		self.uploadFile()
-	
-	def exit(self, code):
-		QCoreApplication.exit(code)
-
-parser = argparse.ArgumentParser(description="Upload file(s) and print links")
-parser.add_argument('-c', '--class', dest='file_class', default='General', help="Use settings from specified class")
-parser.add_argument('-d', '--dir', dest='subdir', default='', help="Upload files to specified subdirectory")
-parser.add_argument('-s', '--hash-name', action='store_true', dest='hash', help="Hash file name for every file")
-parser.add_argument('file', metavar='FILE', nargs='+', help="File(s) to upload")
-args = parser.parse_args()
-
-QCoreApplication.setOrganizationName("HaveFun.cz")
-QCoreApplication.setOrganizationDomain("havefun.cz")
-QCoreApplication.setApplicationName("HaveUp")
-
-notify2.init('HaveUp')
-
-app = QCoreApplication(sys.argv)
-
-hup = HaveUp(args)
-
-sys.exit(app.exec_())
+	HaveUp(args)
